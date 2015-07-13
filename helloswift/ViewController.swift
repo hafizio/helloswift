5920e13eec8978b66a28cb56c27d0d7e69026e1a
Hafiz Khairudin <ibnukamy@gmail.com>
2015-07-13 15:59:18 +0800

    Initial Commit

---
+++ helloswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloswift
+//
+//  Created by Hafiz Khairudin on 7/13/15.
+//  Copyright © 2015 Hafiz Khairudin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
